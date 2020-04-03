29362cb31db92e6ed45e13c4c104e18c936ef23e
wahyukhairina <wkhairina.afzan@gmail.com>
2020-04-03 07:19:03 +0700

    Updates

---
+++ precache-manifest.34355bb4404efb8aaa7e785875749a97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb82143862bb441a2bc53698237a1828",
+    "url": "/wahyukhairina/pos_redux_backup/index.html"
+  },
+  {
+    "revision": "b2875e26d7e27ba29201",
+    "url": "/wahyukhairina/pos_redux_backup/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "21de5047b430dbc77a97",
+    "url": "/wahyukhairina/pos_redux_backup/static/css/main.8dd14b83.chunk.css"
+  },
+  {
+    "revision": "b2875e26d7e27ba29201",
+    "url": "/wahyukhairina/pos_redux_backup/static/js/2.436a9f41.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/wahyukhairina/pos_redux_backup/static/js/2.436a9f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21de5047b430dbc77a97",
+    "url": "/wahyukhairina/pos_redux_backup/static/js/main.66f2c45a.chunk.js"
+  },
+  {
+    "revision": "3e278bdf5cfc56763255",
+    "url": "/wahyukhairina/pos_redux_backup/static/js/runtime-main.7b721608.js"
+  },
+  {
+    "revision": "fd92636084aeeb77ae7ec1f6b3971f25",
+    "url": "/wahyukhairina/pos_redux_backup/static/media/bg.fd926360.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wahyukhairina/pos_redux_backup/precache-manifest.34355bb4404efb8aaa7e785875749a97.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wahyukhairina/pos_redux_backup/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
